acd87663c716a3426c81277e5f6a45c5908ca879
danweinstock <dan.weinstock@yahoo.com>
2015-01-21 16:18:24 -0500

    No we don't need no bananas

--- exercise6.rb
+++ exercise6.rb
@@ -1,4 +1,12 @@
 grocery_list = ["eggs", "bacon", "coffee", "milk"]
+grocery_list<<"rice"
 grocery_list.each do |x|
 	puts "* #{x}"
+end
+puts grocery_list.length
+grocery_list.include?("banana")
+if true
+	puts "You don't need to pick up bananas today"
+else
+	puts "You need to pick up bananas."
 end
