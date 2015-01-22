9237707a93420d214f23e7b513c46c40d54da5cf
danweinstock <dan.weinstock@yahoo.com>
2015-01-21 21:42:45 -0500

    Cohort4 added

--- exercise7.rb
+++ exercise7.rb
@@ -1,5 +1,9 @@
 students = {
 	:cohort1 => 34,
 	:cohort2 => 42,
-	:cohort3 => 22
+	:cohort3 => 22 
 }
+students[:cohort4] = 43
+students.each do |key, value|
+ puts  "#{key}: #{value} students"
+end
