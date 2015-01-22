57a8e187bd1f95f71207d8c72c27fe6c4251da1f
danweinstock <dan.weinstock@yahoo.com>
2015-01-21 22:42:47 -0500

    Exercise 7 except question 5

--- exercise7.rb
+++ exercise7.rb
@@ -4,9 +4,13 @@ students = {
 	:cohort3 => 22 
 }
 students[:cohort4] = 43
-students.each do |key, value|
- puts  "#{key}: #{value} students"
+students.each do |k, v|
+ puts  "#{k}: #{v} students"
 
 end
 puts students.keys
-
+# new_students = students[students.map { |k, v| [k, v * 1.05]}]
+# puts new_students
+puts "Deleting the second cohort"
+students.delete(:cohort2)
+puts students
